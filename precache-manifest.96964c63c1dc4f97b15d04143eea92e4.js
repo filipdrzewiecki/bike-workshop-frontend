9c5d33f2c5ecba1e6e404bef1ce0e409d1e5dcb5
Filip Drzewiecki <filip.drzewiecki@jit.team>
2020-02-16 12:48:02 +0100

    Updates

---
+++ precache-manifest.96964c63c1dc4f97b15d04143eea92e4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834db326306fb826c966c09be585a945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b93fb15250af5bdd2fd",
+    "url": "/static/css/main.afeede5b.chunk.css"
+  },
+  {
+    "revision": "ef756f8093826744c6bb",
+    "url": "/static/js/2.f1894440.chunk.js"
+  },
+  {
+    "revision": "9b93fb15250af5bdd2fd",
+    "url": "/static/js/main.49362c9e.chunk.js"
+  },
+  {
+    "revision": "64e8c0b9607b61c303fe",
+    "url": "/static/js/runtime-main.7c21cecf.js"
+  },
+  {
+    "revision": "f90acdbb677cbda753a4f5716c012bca",
+    "url": "/static/media/BikeLogo.f90acdbb.png"
+  },
+  {
+    "revision": "a132c81371a89d7ec4d8f40226e35efd",
+    "url": "/static/media/CampagnoloFreewheel.a132c813.jpeg"
+  },
+  {
+    "revision": "f99c618c7daae725a679bb3845a00b42",
+    "url": "/static/media/EC.f99c618c.jpg"
+  },
+  {
+    "revision": "1b135ffc1188c87fd103eee65e292f25",
+    "url": "/static/media/IS.1b135ffc.jpg"
+  },
+  {
+    "revision": "ed9df5515c76f600425e9c6b2f037b72",
+    "url": "/static/media/ShimanoFreewheel.ed9df551.jpeg"
+  },
+  {
+    "revision": "bbeb490be514e643db018da67c77abc4",
+    "url": "/static/media/ShimanoSplineFreewheel.bbeb490b.jpeg"
+  },
+  {
+    "revision": "ab16bd298744a4414574173c0a45b316",
+    "url": "/static/media/SramFreewheel.ab16bd29.jpg"
+  },
+  {
+    "revision": "a9b396f2c20a8c9b88b9af85674381ad",
+    "url": "/static/media/Thread.a9b396f2.jpg"
+  },
+  {
+    "revision": "f0115a1d4bd4c2b44fcef37a293d1d6f",
+    "url": "/static/media/ZS.f0115a1d.jpg"
+  },
+  {
+    "revision": "8a553af53418b5d5ef1f30243906004f",
+    "url": "/static/media/companyLogo.8a553af5.png"
+  },
+  {
+    "revision": "c8e608328b90498fc4f765d72018ca77",
+    "url": "/static/media/drivetrain.c8e60832.png"
+  },
+  {
+    "revision": "408c5607bd1223c77379aa711367b657",
+    "url": "/static/media/frame.408c5607.png"
+  },
+  {
+    "revision": "76ea7450da4b272d5a4a3f248355ca3a",
+    "url": "/static/media/hub.76ea7450.png"
+  },
+  {
+    "revision": "be36551f8503be8532b51b1a2ea39b4d",
+    "url": "/static/media/wheel.be36551f.png"
+  }
+]);
