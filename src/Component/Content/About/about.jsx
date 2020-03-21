9e70644fe332a73b0d38bcd15e611bde2c35079c
Filip Drzewiecki <filip.drzewiecki@jit.team>
2020-03-21 21:28:06 +0100

    about section

--- src/Component/Content/About/about.jsx
+++ src/Component/Content/About/about.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import '../../Css/index.css';
+import React from 'react';
+
+import ContentTile from '../../Page/ContentComponents/contentTile';
 import Page from '../../Page/PageElements/article.jsx';
 
+import FilPhoto from '../../../resources/creators/fil.png';
+import LukePhoto from '../../../resources/creators/luke.png';
 
-const paragraphs = ['aaa', 'bbb', 'ccc']
 
-const About = () => {
-    return (
-        <div>
-        <Page 
-            menu1="serws"
-            menu2="serws"
-            menu3="serwis"
+const paragraphs = [
+    <div>
+        Osoby tworzące tę stronę, to wieloletni zapaleńcy, którzy zęby zjedli na naprawach zarówno tych najdroższych , jak i najtańszych i najbardziej
+        budżetowych rowerów. Jeśli masz jakieś sugestie odnośnie tego, jak działa ta strona, uwagi odnośnie prezentowanych tutaj artykułów, 
+        lub po prostu chciałbyś podzielić się swoją opinią, uderzaj na email info@bike-workshop.com. 
 
-            title="Sekcja o nas"
-            paragraphs={paragraphs}
-        />
-        </div>
-    );
+    </div>,
+    <ContentTile
+        imageSource={FilPhoto}
+        name="Filip"
+        content={<div>
+            Opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis
+            opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis
+            opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis
+    </div>} />,
+    <ContentTile
+        imageSource={LukePhoto}
+        name="Łukasz"
+        content={<div>
+            Opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis
+            opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis
+            opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis opis
+    </div>} />
+]
+
+export default class Compatibility extends React.Component {
+    render() {
+        return (
+            <div>
+                <Page
+                    title="My, pedalarze"
+                    paragraphs={paragraphs}
+                />
+            </div>
+        );
+    }
 }
- 
-ReactDOM.render(
-    <About/>,
-    document.querySelector('#root')
-);
-
-export default About;
