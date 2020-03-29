b7cdd83d5b7428fd8bc30b420b4010f41f7166eb
Filip Drzewiecki <filip.drzewiecki@jit.team>
2020-03-29 17:01:26 +0200

    typescript uninstall

--- src/JavaScript/Page/PageElements/poll.jsx
+++ src/JavaScript/Page/PageElements/poll.jsx
@@ -1,3 +1,4 @@
+import '../../Css/index.css';
 import React, { Component } from 'react';
 import Poll from 'react-polls';
  
@@ -9,32 +10,34 @@ const pollAnswers = [
   { option: 'Trzy', votes: 1 },
   { option: 'Cztery', votes: 1 },
   { option: 'Pięć', votes: 1 },
-  { option: 'Sześć lub więcej', votes: 2 }
+  { option: 'Sześć lub więce', votes: 1 }
 ]
  
 export default class VotingPoll extends Component {
-  // Setting answers to state to reload the component with each vote
-  state = {
-    pollAnswers: [...pollAnswers]
-  }
- 
-  handleVote = voteAnswer => {
-    const { pollAnswers } = this.state
-    const newPollAnswers = pollAnswers.map(answer => {
-      if (answer.option === voteAnswer) answer.votes++
-      return answer
-    })
-    this.setState({
-      pollAnswers: newPollAnswers
-    })
-  }
- 
-  render () {
-    const { pollAnswers } = this.state
-    return (
-      <div>
-        <Poll question={pollQuestion} answers={pollAnswers} onVote={this.handleVote} />
-      </div>
-    );
-  }
-};
+    // Setting answers to state to reload the component with each vote
+    state = {
+      pollAnswers: [...pollAnswers]
+    }
+   
+    // Handling user vote
+    // Increments the votes count of answer when the user votes
+    handleVote = voteAnswer => {
+      const { pollAnswers } = this.state
+      const newPollAnswers = pollAnswers.map(answer => {
+        if (answer.option === voteAnswer) answer.votes++
+        return answer
+      })
+      this.setState({
+        pollAnswers: newPollAnswers
+      })
+    }
+   
+    render () {
+      const { pollAnswers } = this.state
+      return (
+        <div>
+          <Poll question={pollQuestion} answers={pollAnswers} onVote={this.handleVote} />
+        </div>
+      );
+    }
+  };
