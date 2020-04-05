f90dd70b5a8d1cb881eedd70af378983e9015326
Filip Drzewiecki <filip.drzewiecki@jit.team>
2020-04-05 19:43:14 +0200

    drivetrain worn 2/2 & tyre worn 1/1

--- src/JavaScript/Content/Maintenance/Articles/drivetrain-worn.jsx
+++ src/JavaScript/Content/Maintenance/Articles/drivetrain-worn.jsx
@@ -4,6 +4,8 @@ import ArticleBody from '../../../Page/PageElements/article.jsx';
 import Section from '../../../Page/PageElements/section.jsx';
 import ChainChecker from '../../../../resources/drivetrain/chainChecker.PNG';
 import ChainCheckerInAction from '../../../../resources/drivetrain/chainCheckerInAction.PNG';
+import ChainringWornout from '../../../../resources/drivetrain/chainringWornout.PNG';
+
 
 
 
@@ -35,12 +37,29 @@ const section2 = [
     </div>,
     <div>
         <img className="photo-medium" src={ChainCheckerInAction} alt="chainCheckerInAction"></img>
+    </div>,
+    <div>
+        Zaprezentowany powyżej przyrząd parktool CC-3.2, który wskazuje 0.5% oraz 0.75% zużycia łańcucha. W zasadzie każdy producent łańcuchów ma własne wytyczne, jaki % zużycia
+        jest właściwym momentem do wymiany i w razie wątpliwości polecam sprawdzić jakiej firmy masz kompontenty i poszukać w dokumentacji. Bezpiecznie jest natomiast przyjąć, że
+        łańcuchy do 10 rzędów dobrze jest zmienić przy 0.75, natomiast 11 rzędowe przy 0.5.
     </div>
 ]
 
 const section3 = [
     <div>
-        Treść artykułu.
+        Niestety nie ma żadnego narzędzia, które mierzyłoby dokładny poziom zużycia kasety lub zębatek korby. Natomiast jest kilka sposób, aby chociaż mniej więcej ocenić, czy
+        napęd nadaje się do wymiany. Po pierwsze, jeżeli łańcuch jest zużyty dość mocno, to możemy z całą pewnością założyć, że reszta też jest w podobnym stanie. Przymiar, który wskazuje
+        0.75% i więcej powinien już być alarmującym sygnałem. Dodatkowo, możemy spojrzeć jak łańcuch układa się na zębatkach z tyłu i z przodu. Jeżeli nie siedzi ciasno, tylko zaznaczają się
+        wyraźne przerwy, tak jak na zdjęciu poniżej, to znaczy, że któryś komponent umiera.
+    </div>,
+    <div>
+        <img className="photo-medium" src={ChainringWornout} alt="ChainringWornout"></img>
+    </div>,
+    <div>
+        Na sam koniec warto poruszyć jeszcze jedną ważną kwestię - jeżeli wszystkie znaki na niebie, ziemi i przymiarze wskazują na to, że masz zużyty cały napęd, a nie tylko łańcuch, to
+        nie zgrywaj Poznaniaka i nie staraj się zaoszczędzić, kupując wyłącznie łańcuch. Nowy łańcuch na zużytej kasecie będzie przeskakiwał między ogniwami - przy każdym większym obciążeniu będzie
+        się po prostu ślizgał i zużyje się w mgnieniu oka. W takiej sytuacji lepiej jest albo kupić cały napęd nowy ALBO nie wymieniać go w ogóle. Dlaczego? Skoro zużyty działa sam ze sobą dość dobrze, 
+        to możesz już zajechać go na amen i dopiero wtedy wymienić całość. Wymiana samego łańcucha, w przypadku, gdy kaseta również nadaje się na śmietnik jest tylko wyrzucaniem pieniędzy w błoto. 
     </div>
 ]
 
--- src/JavaScript/Content/Maintenance/Articles/tyre-worn.jsx
+++ src/JavaScript/Content/Maintenance/Articles/tyre-worn.jsx
@@ -1,11 +1,59 @@
 import '../../../Css/index.css';
 import React from 'react';
+import Section from '../../../Page/PageElements/section.jsx';
 import ArticleBody from '../../../Page/PageElements/article.jsx';
+import ContinentalWornout from '../../../../resources/wheel/tyre/continentalWornout.PNG';
+import TyreLowPreassureWornout from '../../../../resources/wheel/tyre/tyreLowPreassureWornout.PNG';
 
-const paragraphs = [
+const section1 = [
     <div>
-        Treść artykułu.
+        Stopień zużycia opon jest w stanie ocenić każdy i to w domowych warunkach, jeśli tylko zna kilka podstawowych reguł. Te natomiast
+        mogą się od siebie różnić, w zależności od typu roweru, opon oraz nawierzchni po której jeździsz. Czasami może wydawać się, że opona jest już do wyrzucenia, ale
+        tak naprawdę posłuży jeszcze wiele kilometrów. Zacznijmy więc od identyfikacji typu opon, które masz. Przyjmijmy, że są trzy typy - [1] typowe górskie, z wyraźnym bieżnikiem i kostką,
+        [2] crossowe, lub semi-slicki, które bieżnikiem mocno przypominają opony samochodowe - gładne, ale z licznymi wcięciami i "wzorkami", oraz [3] slicki, czyli zupełnie gładkie opony, spotykane
+        zasadniczo wyłącznie w rowerach szosowych.
+    </div> 
+]
+
+const section2 = [
+    <div>
+        Opony górskie to wielki balon z wieloma kostkami, które zapewniają odpowiednią trakcję na miękkim podłożu. Najłatwiej rozpoznać stopień zużycia takiej opony, obserwując bieżnik
+        oraz sposób w jaki zmienia się wraz z coraz to kolejnymi wyjazdami. Jeżeli krawędzie kostek stają się mniej wyraźne, lub są wręcz poobrywane, to znak, że ich czas jest blisko.
+        Jeżdżąc na miękkich oponach po zbyt twardym podłożu może wręcz się zdarzyć, że ząbku zupełnie się pourywają, zostawiając nieładne fragmenty drutu.
+    </div>,
+    <div>
+        Bardzo popularnym defektem jest jazda na kołach o zbyt niskim ciśnieniu. Wtedy na ściankach bocznych opony można zauważyć wychodzące włókna, tak jak na obrazku poniżej. Nie musi
+        to oczywiście oznaczać, że opona jest do wyrzucenia, ale jeżeli ścianki są już zbyt mocno zniszczone, taka opona może eksplodować.
+    </div>,
+    <div>
+        <img className="photo-horizontal" src={TyreLowPreassureWornout} alt="tyreLowPreassureWornout"></img>
     </div>
+]
+
+const section3 = [
+    <div>
+        W przypadku tych opon sprawa jest dosć prosta, ponieważ wystarczy doglądać tego, czy opona pół-gładka nie stała się oponą gładką ;) Jeżeli wcięcia, rowki oraz wzory zanikają,
+        to oznaka, że starła się już zbyt duża warstwa gumy.
+    </div>
+]
+
+const section4 = [
+    <div>
+        Nowa opona typu slick zawsze będzie mieć ładny, okrągły profil. Zużywa się poprzez stopniowe ścieranie się kolejnych warstw gumy, poczynając od samego czubka.
+        Jeżeli więc profil przestaje być okrągły, a zaczyna czubek jest "ścięty", opona jest do wymiany. Wielu producentów dodatkowo oznacza stopień zużycia opon. Poniżej przykład
+        z opon continental, mających charakterystyczne dwa rowki. Jeżeli rowki w pewnym momencie znikną, to znaczy, że guma starła się już ponad normę i należy udać się do sklepu
+        po nowe egzemplarze.
+    </div>,
+    <div>
+        <img className="photo-horizontal" src={ContinentalWornout} alt="continentalWornout"></img>
+    </div>
+]
+
+const content = [
+    <Section paragraphs={section1} />,
+    <Section paragraphs={section2} title={'Opony górskie'} />,
+    <Section paragraphs={section3} title={'Opony crossowe'} />,
+    <Section paragraphs={section4} title={'Opony gładkie'} />
 ]
 
 export default class Article extends React.Component {
@@ -14,7 +62,7 @@ export default class Article extends React.Component {
             <div>
                 <ArticleBody
                     title="Zużycie opon"
-                    paragraphs={paragraphs}
+                    paragraphs={content}
                     backButtonLink='/maintenance'
                 />
             </div>
